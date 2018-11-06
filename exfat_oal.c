2ac42ac44af6e8307e2cdb38febbb6e2b6f9918c
coolshou <coolshou2060@gmail.com>
2018-11-06 10:34:55 +0800

    fix kernel 4.18 compile error

--- exfat_oal.c
+++ exfat_oal.c
@@ -112,13 +112,21 @@ static time_t accum_days_in_year[] = {
 
 TIMESTAMP_T *tm_current(TIMESTAMP_T *tp, UINT8 tz_utc)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,18,0)
+	struct timespec64 ts;
+#else
 	struct timespec ts;
+#endif
 	time_t second, day, leap_day, month, year;
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,8,0)
 	ts = CURRENT_TIME_SEC;
 #else
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,18,0)
+	ktime_get_real_ts64(&ts);
+#else
 	ktime_get_real_ts(&ts);
+#endif
 #endif
 
 	second = ts.tv_sec;
